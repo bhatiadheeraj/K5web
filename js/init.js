7acc596168e6ee73c6cfcb892138972bf74c5c31
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2018-07-27 04:41:17 +0530

    Rough Draft
    Building the code

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+          
